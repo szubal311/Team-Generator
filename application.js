dfd2c811f2a6d2f28555fb568fbaba2efb33ea4f
szubal311 <73494903+szubal311@users.noreply.github.com>
2021-02-25 18:00:45 -0700

    added js file

---
+++ application.js
@@ -0,0 +1,67 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const manager = require();
+const engineer = require();
+const intern = require();
+const employees = [];
+
+appInitialize = () => {
+    launchHtml();
+    addEmplyee();
+}
+addEmployee = () => {
+    inquirer.prompt([{
+        message: "Enter Name of Team Member",
+        name: "name"
+    },
+
+    {
+        type: "list",
+        message: "Select the team member's role",
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern",
+        ],
+        
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id",
+    },
+    {
+        message: "Enter team member's email",
+        name: "email"
+    }
+]).then(({name, role, id, email}) => {
+    let roleData = "";
+    if (role === "Manager") {
+        roleData = "office phone number"
+    } else if (role === "Engineer") {
+        roleData = "GitHub username"
+    } else {
+        roleData ="school name"
+    }
+
+})
+
+    inquirer.prompt([{
+        message: `Enter team menmber's ${roleData}`,
+        name: "roleData"
+    },
+    {
+        type: "list",
+        message: "Would you like to add more team members?"
+        choices:[
+            "yes",
+            "no"
+        ],
+        name: "membersAdded"
+    }]).then(({roleData, membersAdded}) =>{
+        
+    })
+    
+
+
+}
